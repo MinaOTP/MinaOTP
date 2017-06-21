da4c580c20613e65e6ba76dc207161838a6b04cf
lancegin <gin.lance.inside@hotmail.com>
2017-06-21 16:12:05 +0800

    upd: simplify the tokenjs

--- pages/token/token.js
+++ pages/token/token.js
@@ -15,6 +15,11 @@ var tokens = [
     issue: 'Server',
     remark: 'gin@test',
     digit: '534154'.split("")
+  },
+  {
+    issue: 'Server',
+    remark: 'gin@mrfan',
+    digit: '423111'.split("")
   }
 ]
 
@@ -32,55 +37,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    TOTP.sayHello("hfd");
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    let digit = TOTP.now("hjfdshgkjdshgkl")
+    // console.log(digit)
   }
 })
