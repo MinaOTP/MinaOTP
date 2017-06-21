1d9ffde994b10eb8c7865d355766e54a83abefec
lancegin <gin.lance.inside@hotmail.com>
2017-06-21 14:14:58 +0800

    upd: bind the data to server

--- pages/token/token.js
+++ pages/token/token.js
@@ -1,18 +1,38 @@
 // pages/token/token.js
+var TOTP = require('../../utils/totp')
+var tokens = [
+  {
+    issue: 'Google',
+    remark: 'gin.lance.inside@hotmail.com',
+    digit: '434342'.split("")
+  },
+  {
+    issue: 'Github',
+    remark: 'LanceGin',
+    digit: '111232'.split("")
+  },
+  {
+    issue: 'Server',
+    remark: 'gin@test',
+    digit: '534154'.split("")
+  }
+]
+
 Page({
 
   /**
    * 页面的初始数据
    */
+
   data: {
-  
+    tokens: tokens
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    TOTP.sayHello("hfd");
   },
 
   /**
