0ab10e1edcb3835f87c88c95da1b1be9e11562aa
lancegin <gin.lance.inside@hotmail.com>
2017-06-23 17:46:10 +0800

    add: parseURL function

--- utils/util.js
+++ utils/util.js
@@ -16,6 +16,32 @@ function formatNumber(n) {
   return n[1] ? n : '0' + n
 }
 
+// 解析url
+function parseURL(url) {
+  let url_a = url.split("?")
+  if (1 == url_a.length) {
+    return {
+      params: null
+    }
+  } else {
+    let search = url_a[1]
+    return {
+      params: (function () {
+        var ret = {},
+          seg = search.replace(/^\?/, '').split('&'),
+          len = seg.length, i = 0, s;
+        for (; i < len; i++) {
+          if (!seg[i]) { continue; }
+          s = seg[i].split('=');
+          ret[s[0]] = s[1];
+        }
+        return ret;
+      })()
+    }
+  }
+}   
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  parseURL: parseURL
 }
