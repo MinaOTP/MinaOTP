7fbf7dedbf180cb4d2bdac25d2e657307218f9eb
lancegin <gin.lance.inside@hotmail.com>
2017-06-23 16:03:05 +0800

    add: add digit function

--- pages/form/form.js
+++ pages/form/form.js
@@ -1,4 +1,5 @@
 // pages/form/form.js
+let token = []
 Page({
 
   /**
@@ -27,13 +28,58 @@ Page({
    */
   keySubmit: function (e) {
     let values = e.detail.value
-    console.log(values)
     if ("" == values.key) {
       wx.showModal({
         content: '忘记KEY了？',
         showCancel: false,
         confirmText: '返回',
         confirmColor: '#ff9c10',
+      })
+    } else {
+      let token_obj = {
+        issue: values.service,
+        remark: values.account,
+        token: values.key
+      }
+      console.log(token_obj)
+      // 获取缓存的token数组
+      wx.getStorage({
+        key: 'token',
+        success: function (res) {
+          token = res.data
+          // 更新缓存的token信息
+          token.push(token_obj)
+          // 更新缓存
+          wx.setStorage({
+            key: 'token',
+            data: token,
+            success: function(res) {
+              console.log(res)
+            },
+            fail: function(res) {
+              console.log(res)
+            },
+          })
+        },
+        fail: function (res) {
+          // 缓存中不存在token时获取初始数组
+          token.push(token_obj)
+          wx.setStorage({
+            key: 'token',
+            data: token,
+            success: function(res) {
+              console.log(res)
+            },
+            fail: function(res) {
+              console.log(res)
+            },
+          })
+        },
+        complete: function () {
+          wx.navigateBack({
+            delta: 1,
+          })
+        }
       })
     }
   }
